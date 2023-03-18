8763c3bffdcaf02dfb024f489910675c97a933cb
impasta237 <abirdsong237@gmail.com>
2023-03-17 19:56:50 -0500

    add clear function

--- calculator.js
+++ calculator.js
@@ -20,11 +20,32 @@ let answerDisplay = document.querySelector('.answer-display');
 function updateDisplay(string) {
     display=display+string;
     answerDisplay.textContent = display;
-    console.log(display);
     return display;
 }
 
+function clearDisplay() {
+    answerDisplay.textContent = "";
+    display = "";
+}
+
 addEventListener('click', (e) => {
-    let number = e.target.getAttribute('id');
-    updateDisplay(number);
+    let input = e.target.getAttribute('id');
+    let type = e.target.getAttribute('class');
+    
+    switch (type) {
+        case "clear" :
+            clearDisplay();
+            break;
+        case "number" :
+            updateDisplay(input);
+            break;
+        case "operator" :
+            updateDisplay(input);
+            break;
+        case "equal" :
+            updateDisplay(input);
+            break;
+        default :
+            break;
+    }
 });
